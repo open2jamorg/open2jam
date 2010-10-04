ebc74c1ce2f6a05742c6e8fffae04e861ef63452
chaosfox <fox@lightning.localdomain>
2010-10-03 23:08:34 -0300

    new parser, now output png instead

--- ojnparser.pl
+++ ojnparser.pl
@@ -1,47 +1,24 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Data::Dumper;
-$|++;
+use GD;
 
 
 my $filename = shift;
 
-my $speed = 2;
-my $unk_beat = 8*$speed; # note distribution in the beat
-#
-# unk_beat = 8 -- x1 speed !!
-#
-my $beatsize = 4*$unk_beat; # beat spacing size
+my $sub_beats = 6; # note distribution in the beat
+
+my $sub_beat_size = 50; # beat spacing size
 
 my @lvll = ('[Ex]','[Nx]','[Hx]');
+
+
 my $NOTEPAD = 100; # left side padding
 my $notelevel = 2; # 0,1,2 <-> E,N,H
-my $notesize = 10; #7 vertical size of a note
-my $notewidth = 50; #20 horizontal size of a note
+my $note_height = 7; # vertical size of a note
+my $note_width = 45; # horizontal size of a note
 my $csize = 200; # comments space size
 
-
-
-my @color;
-my $black  = "rgb(  0,  0,  0)";  # black
-$color[0]  = "rgb(255,255,255)";  # white
-$color[1]  = "rgb(241,241,243)";  # white
-$color[2]  = "rgb( 68,212,246)";  # cyan
-$color[3]  = "rgb(254,204, 83)";  # yellow
-$color[4]  = "rgb( 50, 50, 50)";  # dark gray
-$color[5]  = "rgb(120,120,120)";  # gray
-$color[6]  = "rgb(255,150,150)";  # pink
-$color[7]  = "rgb(180,180,180)";  # light gray
-$color[11] = "rgb(193,193,194)";  # light gray
-$color[12] = "rgb( 54,170,197)";  # light blue
-$color[13] = "rgb(203,163, 66)";  # light brown
-$color[21] = "rgb(248,243,247)";  # white
-$color[22] = "rgb(130,231,241)";  # light cyan
-$color[23] = "rgb(255,246,157)";  # light yellow
-$color[31] = "rgb(198,194,198)";  # light gray
-$color[32] = "rgb(104,185,193)";  # cyan
-$color[33] = "rgb(204,197,126)";  # light yellow
 
 
 my %map  = (
@@ -84,7 +61,6 @@ l3:@time
 l4:@notepos
 /), $data);
 
-#die Dumper $h;
 
 my @notepos = @{$h->{'notepos'}};
 
@@ -96,7 +72,7 @@ my ($startpos,$endpos) = ($notepos[$notelevel],$notepos[$notelevel + 1]);
 seek DATA, $startpos, 0; # go to pos where lvl starts
 
 my @note;
-my $maxbeat = 0;
+my $total_sub_beats = 0;
 while(!eof DATA && tell DATA < $endpos)
 {
 	read DATA, $data, 8;
@@ -118,8 +94,8 @@ while(!eof DATA && tell DATA < $endpos)
 
 			next if($value[0] == 0);
 
-			my $beat = (4 * ($measure + ($i / $length)));
-			$maxbeat = $beat if ($beat > $maxbeat);
+			my $beat = ($sub_beats * ($measure + ($i / $length)));
+			$total_sub_beats = $beat if ($beat > $total_sub_beats);
 
 			my $mychan;
 			if($channel == 1)
@@ -135,48 +111,65 @@ while(!eof DATA && tell DATA < $endpos)
 			'value'   => $value[0],
 			'rawchan' => $channel
 			};
+			printf STDERR "%03.2f -- chan(%02d) ".($beat / $sub_beats)."\n",$beat,$mychan;
 		}
 	}else{
 		seek DATA, 4 * $length, 1; ## jumping what ??
 
 	}
 }
+$total_sub_beats = int($total_sub_beats+0.5);
 
-my ($width,$height) = (($notewidth * 7) + 2 * $NOTEPAD, ($maxbeat * $beatsize) + $csize);
+my ($width,$height) = (($note_width * 7) + 2 * $NOTEPAD, ($total_sub_beats * $sub_beat_size) + $csize);
 
-print qq(<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+my $im = GD::Image->new($width, $height);
 
-<svg width="$width" height="$height"
-style="background-color:black;" version="1.1"
-xmlns="http://www.w3.org/2000/svg">\n\n);
+my @color;
+my $black  = $im->colorAllocate(  0,  0,  0);  # black
+$color[0]  = $im->colorAllocate(255,255,255);  # white
+$color[1]  = $im->colorAllocate(241,241,243);  # white
+$color[2]  = $im->colorAllocate( 68,212,246);  # cyan
+$color[3]  = $im->colorAllocate(254,204, 83);  # yellow
+$color[4]  = $im->colorAllocate( 50, 50, 50);  # dark gray
+$color[5]  = $im->colorAllocate(120,120,120);  # gray
+$color[6]  = $im->colorAllocate(255,150,150);  # pink
+$color[7]  = $im->colorAllocate(180,180,180);  # light gray
+$color[11] = $im->colorAllocate(193,193,194);  # light gray
+$color[12] = $im->colorAllocate( 54,170,197);  # light blue
+$color[13] = $im->colorAllocate(203,163, 66);  # light brown
+$color[21] = $im->colorAllocate(248,243,247);  # white
+$color[22] = $im->colorAllocate(130,231,241);  # light cyan
+$color[23] = $im->colorAllocate(255,246,157);  # light yellow
+$color[31] = $im->colorAllocate(198,194,198);  # light gray
+$color[32] = $im->colorAllocate(104,185,193);  # cyan
+$color[33] = $im->colorAllocate(204,197,126);  # light yellow
+
 
 
 # vertical lines, delimiter the note space
 for my $i(0 .. 7)
 {
-	my $x = $NOTEPAD + ($i * $notewidth);
-	print line($x, 0, $x, $height, $color[4]);
+	my $x = $NOTEPAD + ($i * $note_width);
+	$im->line($x, 0, $x, $height, $color[4]);
 }
-print line($NOTEPAD, 0, $NOTEPAD, $height, $color[5]); 
-print line($NOTEPAD + (7 * $notewidth), 0, $NOTEPAD + (7 * $notewidth), $height, $color[5]);
+$im->line($NOTEPAD, 0, $NOTEPAD, $height, $color[5]); 
+$im->line($NOTEPAD + (7 * $note_width), 0, $NOTEPAD + (7 * $note_width), $height, $color[5]);
 
 
 ## horizontal lines
-for my $i(0 .. $maxbeat)
+for my $i(0 .. $total_sub_beats)
 {
-	my $x2 = $NOTEPAD + (7 * $notewidth);
-	my $y  = $height - (($beatsize * $i) + $notesize);
-	print line($NOTEPAD, $y, $x2, $y, $color[4]);
-	if ($i % $unk_beat == 0)
+	my $x2 = $NOTEPAD + (7 * $note_width);
+	my $y  = $height - ($sub_beat_size * $i);
+	$im->line($NOTEPAD, $y, $x2, $y, $color[4]);
+	if ($i % $sub_beats == 0) # beat mark
 	{
-		print line($NOTEPAD, $y - 1, $x2, $y - 1, $color[5]);
-		print string(12, $NOTEPAD + 4 + (7 * $notewidth), $y - 10, sprintf("#%03d",$i/$unk_beat), $color[5]);
+		$im->line($NOTEPAD, $y - 1, $x2, $y - 1, $color[5]);
+		$im->string(gdSmallFont, $NOTEPAD + (7 * $note_width) + 4, $y - 10, sprintf("#%03d",$i/$sub_beats), $color[5]);
 	}
 }
 
-my @lch; # store long notes
+my @lch; # to store long notes
 my %stat = (
     'tap'  => 0,
     'long' => 0,
@@ -184,18 +177,16 @@ my %stat = (
     'max'  => $bpm
 );
 
-print comment("start notes");
-
 foreach my $v(@note)
 {
 	my $channel = $v->{'channel'};
 	if($channel eq 'BZM') # bpm changing ??
 	{
 		my $newbpm = $v->{'value'};
-		my $y = $height - ($beatsize * $v->{'beat'}) + $notesize;
+		my $y = $height - ($sub_beat_size * $v->{'beat'}) - $note_height;
 		my $text = sprintf "%.2f", $newbpm;
 
-		print string(5, $NOTEPAD - (length($text) * 9)-4, $y - 10, $text, $color[6]);
+		$im->string(gdSmallFont, $NOTEPAD - (length($text) * 9)-4, $y - 10, $text, $color[6]);
 
 		$stat{'min'} = $newbpm if ($newbpm < $stat{'min'});
 		$stat{'max'} = $newbpm if ($newbpm > $stat{'max'});
@@ -206,11 +197,9 @@ foreach my $v(@note)
 		if ($channel >= 0 && $channel <= 6) # tap note
 		{
 			$stat{'tap'}++;
-			my $x = $NOTEPAD + ($channel * $notewidth);
-			my $y = $height - ($beatsize * $v->{'beat'});
-			print rect($x, $y, $notewidth - 1, $notesize - 1, $color[$c]);
-			print line($x, $y + $notesize - 1, $x + $notewidth - 1, $y + $notesize - 1, $color[10 + $c]);
-			print line($x, $y, $x + $notewidth - 1, $y, $color[10 + $c]);
+			my $x = $NOTEPAD + ($channel * $note_width);
+			my $y = $height - ($sub_beat_size * $v->{'beat'});
+			$im->filledRectangle($x, $y, $x+$note_width, $y-$note_height, $color[$c]);
 		}elsif($channel >= 10 && $channel <= 16) # long note
 		{
 			$stat{'long'}++;
@@ -218,58 +207,29 @@ foreach my $v(@note)
 			{
 				$lch[$channel] = $v->{'beat'};
 			}else{
-				my $x = $NOTEPAD + (($channel - 10) * $notewidth);
-				my $y = $height - ($beatsize * $v->{'beat'});
-				my $z = $height - ($beatsize * $lch[$channel]);
+				my $x = $NOTEPAD + (($channel - 10) * $note_width);
+				my $y = $height - ($sub_beat_size * $v->{'beat'}) - $note_height;
+				my $z = $height - ($sub_beat_size * $lch[$channel]) - $note_height;
 				delete $lch[$channel];
-				print rect($x,$y,$notewidth-1,($z-$y)+$notesize-1, $color[20 + $c]);
-				print rect($x+1,$y,($notewidth*0.15)-1,($z-$y)+$notesize-1, $color[0]);
-				print rect($x+($notewidth*0.85)-1,$y,($notewidth*0.15)-2,($z-$y)+$notesize-1,$color[30+$c]);
-				print line($x,$z+$notesize-1,$x+$notewidth-1,$z+$notesize-1,$color[30 + $c]);
-				print line($x, $y, $x + $notewidth - 1, $y, $color[30 + $c]);
+				$im->filledRectangle($x,$y,$x+$note_width,$z+$note_height, $color[20 + $c]);
 			}
 		}
 	}
 }
 
-## shade comments block
-print line(0, $_, $width, $_, $black) for(grep{$_%2}0 .. $csize);
 
-my $y = 20;print string(5, 24, $y, "$lvll[$notelevel] $artist - $title", $color[0]);
-$y += 20;  print string(3, 24, $y, $noter, $color[1]);
+my $y = 20;$im->string(gdSmallFont, 24, $y, "$lvll[$notelevel] $artist - $title", $color[0]);
+$y += 20;  $im->string(gdSmallFont, 24, $y, $noter, $color[1]);
 $y += 15;
 
 my $st = ' [' . sprintf("%.3d",$stat{'min'}) . '-' . sprintf("%.3d",$stat{'max'}) . ']';
-$y += 2; print string(1, 24, $y, " - Tap Notes:  " . $stat{'tap'},  $color[7]);
-$y += 15;print string(1, 24, $y, " - Long Notes: " . $stat{'long'}, $color[7]);
-$y += 15;print string(1, 24, $y, " - BPM:        " . sprintf("%.3d",$bpm) . $st,  $color[7]);
-
-print "</svg>\n";
+$y += 2; $im->string(gdSmallFont, 24, $y, " - Tap Notes:  " . $stat{'tap'},  $color[7]);
+$y += 15;$im->string(gdSmallFont, 24, $y, " - Long Notes: " . $stat{'long'}, $color[7]);
+$y += 15;$im->string(gdSmallFont, 24, $y, " - BPM:        " . sprintf("%.3d",$bpm) . $st,  $color[7]);
 
 
-
-sub rect
-{
-	my ($x,$y,$wid,$hei,$color) = @_;
-	return qq(<rect x="$x" y="$y" width="$wid" height="$hei" style="fill:$color;"/>\n);
-}
-sub line
-{
-	my($x1,$y1,$x2,$y2,$color) = @_;
-	return qq(<line x1="$x1" y1="$y1" x2="$x2" y2="$y2" style="stroke:$color;stroke-width:1"/>\n);
-}
-sub string
-{
-	my ($font,$x,$y, $string, $color) = @_;
-	$string =~ s/\0//g;
-	return qq(<text x="$x" y="$y" style="fill:$color;font-size:$font">$string</text>\n);
-}
-
-sub comment
-{
-	my ($str) = @_;
-	return qq(<!-- $str -->\n);
-}
+binmode STDOUT;
+print $im->png;
 
 sub unpack2hash
 {
