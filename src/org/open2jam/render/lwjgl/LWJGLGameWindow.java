23888d98ba04596682d84507efa29b54ebbf36e7
chaosfox <fox@lightning.localdomain>
2011-07-02 13:39:08 -0300

    sleep() works better I think, now we need to change the interface

--- src/org/open2jam/render/lwjgl/LWJGLGameWindow.java
+++ src/org/open2jam/render/lwjgl/LWJGLGameWindow.java
@@ -4,7 +4,6 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.locks.LockSupport;
 import java.util.logging.Level;
 import org.open2jam.util.Logger;
 
@@ -352,10 +351,9 @@ public class LWJGLGameWindow implements GameWindow {
         public void run() {
             while(active) {
                 processMessages();
-                LockSupport.parkNanos(1000); //like sleep but in nanos, 1 microsecond
-                //try {sleep(10);} catch (InterruptedException ex) {}
+                try {sleep(5);} catch (InterruptedException ex) {}
             }
-        }       
+        }
         public synchronized void processMessages() {
             Display.processMessages();
             while(Keyboard.next()) {
