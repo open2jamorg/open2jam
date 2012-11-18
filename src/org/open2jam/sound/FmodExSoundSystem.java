da47e87abb6a3d3012e30a5ff8fd0fb3a1bc548e
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-11-18 08:35:29 +0700

    Change number of FMOD software channels from 32 to 512.

--- src/org/open2jam/sound/FmodExSoundSystem.java
+++ src/org/open2jam/sound/FmodExSoundSystem.java
@@ -53,6 +53,7 @@ public class FmodExSoundSystem implements SoundSystem {
         system = new org.jouvieje.fmodex.System();
         errorCheck(FmodEx.System_Create(system));
         errorCheck(system.setDSPBufferSize(1024, 2));
+        errorCheck(system.setSoftwareChannels(512));
         errorCheck(system.init(4093, FMOD_INITFLAGS.FMOD_INIT_NORMAL, null));
         errorCheck(system.getMasterChannelGroup(masterGroup));
         errorCheck(system.createChannelGroup("BGM", bgmGroup));
